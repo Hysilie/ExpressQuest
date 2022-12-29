6e72e1cb0ad21ea1d3cdf4fe7918f45508646937
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-12-29 16:10:50 +0100

    Quest 8

---
+++ Express-Quests/app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+const userHandlers = require("./userHandlers");
+
+const movieHandlers = require("./movieHandlers");
+
+/* public routes */
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+/* protected routes */
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+app.put("/api/movies/:id", verifyToken, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", verifyToken, movieHandlers.deleteMovie);
+app.get("/api/users", verifyToken, userHandlers.getUsers);
+app.get("/api/users/:id", verifyToken, userHandlers.getUserById);
+app.put("/api/users/:id", verifyToken, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyToken, userHandlers.deleteUser);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
